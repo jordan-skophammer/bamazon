c871bd41131d55064fd66d96e25aad57abd00d60
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-06 20:11:15 -0500

    display total works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,51 +15,59 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+    displayInventory();
 });
 
 
-
-    
-connection.query("SELECT * FROM products", function(err, res) {
-    for (let i = 0; i < res.length; i++) {
-    console.log(res[i].item_id + " " + res[i].product_name + " " + res[i].department_name + " " + "$" + res[i].price);
-    }
-    console.log("_____________________________")
-
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "item",
-            message: "Please choose the number of the item you would like to purchase."
-        },
-        {
-            type: "input",
-            name: "quanity",
-            message: "How many items would you like to purchase?"
+function displayInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for (let i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " " + res[i].product_name + " " + res[i].department_name + " " + "$" + res[i].price);
         }
-    ]).then(function customerPO(answer){
-        answer.item =  answer.item - 1
-        var customerItem = res[answer.item].item_id
-        var customerQuanity = answer.quanity
-        var stock_quanity = res[answer.item].stock_quanity
+        console.log("_____________________________")
+    
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "item",
+                message: "Please choose the number of the item you would like to purchase."
+            },
+            {
+                type: "input",
+                name: "quanity",
+                message: "How many items would you like to purchase?"
+            }
+        ]).then(function customerPO(answer){
+            answer.item =  answer.item - 1
+            var customerItem = res[answer.item].item_id
+            var customerQuanity = answer.quanity
+            var productName = res[answer.item].product_name
+            var productPrice = res[answer.item].price
+            var stock_quanity = res[answer.item].stock_quanity
+    
+            itemQuery(customerItem, productName, customerQuanity, productPrice, stock_quanity)
+        })
+    }) 
+}
+    
+  
 
-        itemQuery(customerItem, customerQuanity, stock_quanity)
-    })
-})   
 
-
-function itemQuery(product, quanity, stock) {
+function itemQuery(id, product, quanity, price, stock) {
 
     var updatedStock = stock - quanity;
+    var total = price * quanity;
 
-    // if(updatedStock >= 0) {
-        connection.query("UPDATE products SET stock_quanity = ? WHERE item_id = ?", [updatedStock, product], function(err, res) {
+    if(updatedStock >= 0) {
+        connection.query("UPDATE products SET stock_quanity = ? WHERE item_id = ?", [updatedStock, id], function(err, res) {
             if (err) throw err;
-            console.log(res)
+            console.log("\nYour Order Summary: \n\n " + product + "     Qty: " + quanity + "\n\n Total: $" + total)
         })
 
-    // }
-    console.log(product)
-    console.log(quanity)
-    console.log(updatedStock)
+    }else {
+        console.log("\nSorry, Bamazon has a insufficient quanity to fulfill your order.\n")
+        displayInventory();
+    }
+
+    connection.end();
 }

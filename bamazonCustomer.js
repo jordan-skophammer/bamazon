a7df2a56f44da0cb653eb262dbb9c54828061842
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-06 19:45:41 -0500

    updated stock works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,45 +15,51 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    displayItems();
-    // itemQuery()
 });
 
-function displayItems() {
+
 
     
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (let i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " " + res[i].product_name + " " + res[i].department_name + " " + "$" + res[i].price);
+connection.query("SELECT * FROM products", function(err, res) {
+    for (let i = 0; i < res.length; i++) {
+    console.log(res[i].item_id + " " + res[i].product_name + " " + res[i].department_name + " " + "$" + res[i].price);
+    }
+    console.log("_____________________________")
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item",
+            message: "Please choose the number of the item you would like to purchase."
+        },
+        {
+            type: "input",
+            name: "quanity",
+            message: "How many items would you like to purchase?"
         }
-        console.log("_____________________________")
+    ]).then(function customerPO(answer){
+        answer.item =  answer.item - 1
+        var customerItem = res[answer.item].item_id
+        var customerQuanity = answer.quanity
+        var stock_quanity = res[answer.item].stock_quanity
 
-        // itemQuery()
+        itemQuery(customerItem, customerQuanity, stock_quanity)
+    })
+})   
 
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "item",
-                message: "Please choose the number of the item you would like to purchase."
-            },
-            {
-                type: "input",
-                name: "quanity",
-                message: "How many items would you like to purchase?"
-            }
-        ]).then(function customerPO(answer){
-    
-            console.log("___________")
-            console.log(res[answer.item].product_name)
-            console.log(res[answer.item].stock_quanity)
+
+function itemQuery(product, quanity, stock) {
+
+    var updatedStock = stock - quanity;
+
+    // if(updatedStock >= 0) {
+        connection.query("UPDATE products SET stock_quanity = ? WHERE item_id = ?", [updatedStock, product], function(err, res) {
+            if (err) throw err;
+            console.log(res)
         })
-    })   
+
+    // }
+    console.log(product)
+    console.log(quanity)
+    console.log(updatedStock)
 }
-
-// function itemQuery(price) {
-
-    
-
-    
-// }
-
